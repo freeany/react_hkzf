b1a868a214f9a3deba24a5bc2571b13b1039301d
lhr <812974019@qq.com>
2020-02-24 21:12:35 +0800

    项目优化完成

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { lazy, Component, Suspense } from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
-import Home from './pages/Home'
-import CityList from './pages/CityList'
-import Map from './pages/Map'
-import Profile from './pages/Profile'
-import Detail from './pages/HouseDetail'
-import Login from './pages/Login'
-import Rent from './pages/Rent'
+
+// import Home from './pages/Home'
+// import CityList from './pages/CityList'
+// import Map from './pages/Map'
+// import Profile from './pages/Profile'
+// import Detail from './pages/HouseDetail'
+// import Login from './pages/Login'
+// import Rent from './pages/Rent'
+
 import AuthRoute from './components/AuthRoute'
+
+const Home = lazy(() => import('./pages/Home'))
+const CityList = lazy(() => import('./pages/CityList'))
+const Map = lazy(() => import('./pages/Map'))
+const Profile = lazy(() => import('./pages/Profile'))
+const Detail = lazy(() => import('./pages/HouseDetail'))
+const Login = lazy(() => import('./pages/Login'))
+const Rent = lazy(() => import('./pages/Rent'))
 
 // import { Button } from 'antd-mobile'
 
@@ -16,18 +26,20 @@ class App extends Component {
     return (
       <div className="app">
         <Router>
-          <Route
-            exact
-            path="/"
-            render={() => <Redirect to="/home"></Redirect>}
-          ></Route>
-          <Route path="/home" component={Home}></Route>
-          <Route path="/citylist" component={CityList}></Route>
-          <Route path="/map" component={Map}></Route>
-          <Route path="/detail/:id" component={Detail}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/profile" component={Profile}></Route>
-          <AuthRoute path="/rent" component={Rent}></AuthRoute>
+          <Suspense fallback={<div>组件 努力加载中...</div>}>
+            <Route
+              exact
+              path="/"
+              render={() => <Redirect to="/home"></Redirect>}
+            ></Route>
+            <Route path="/home" component={Home}></Route>
+            <Route path="/citylist" component={CityList}></Route>
+            <Route path="/map" component={Map}></Route>
+            <Route path="/detail/:id" component={Detail}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/profile" component={Profile}></Route>
+            <AuthRoute path="/rent" component={Rent}></AuthRoute>
+          </Suspense>
         </Router>
         {/* <div>
           <Button type="primary">按钮</Button>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App'
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 import './index.css'
-import 'antd-mobile/dist/antd-mobile.css'
+// import 'antd-mobile/dist/antd-mobile.css'
 // 引入字体图标的颜色
 import './asstes/fonts/iconfont.css'
 import 'react-virtualized/styles.css'
